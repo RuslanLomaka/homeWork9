e71aacfe2b4e2bedbbbccf12f12766ca277155ae
margulis <wfptny@gmail.com>
2024-12-09 23:46:30 +0100

    start with linklist

---
+++ src/MyLinkedList.java
@@ -0,0 +1,40 @@
+
+/*
+add(Object value) додає елемент в кінець
+remove(int index) видаляє елемент із вказаним індексом
+clear() очищає колекцію
+size() повертає розмір колекції
+get(int index) повертає елемент за індексом
+*/
+
+
+
+public class MyLinkedList {
+    class Node{
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
